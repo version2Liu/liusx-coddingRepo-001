e1305dc02d376416f4adf6100c1e0d7e7248521f
liushixing <liushixing@chinaport.gov.cn>
2020-06-08 14:55:04 +0800

    20200608 初始化模块 java8及以上新特性实践 多线程研究

---
+++ practice-javase/src/main/java/com/liusx/multithreading/communication/MyConsumer.java
@@ -0,0 +1,10 @@
+package com.liusx.multithreading.communication;
+
+/**
+ * description: MyConsumer <br>
+ * date: 2020/6/8 14:07 <br>
+ * author: liusx <br>
+ * version: 1.0 <br>
+ */
+public class MyConsumer {
+}
