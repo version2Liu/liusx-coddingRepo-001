32b44a710415074f801fce292beeaeadb665cb20
liushixing <450973703@qq.com>
2021-12-19 23:06:46 +0800

    ADD 异或运算记忆练习

---
+++ algorithm/src/test/java/com/algorithm/memory/xor/XORCalculationPractice.java
@@ -0,0 +1,33 @@
+package com.algorithm.memory.xor;
+
+/**
+ * @author ：liusx
+ * @date ：Created in 2021/12/19 23:03
+ * @description：异或运算相关练习
+ * @version: 1.0
+ */
+public class XORCalculationPractice {
+
+    public static void findOddNumTime2(int[] arr){
+        int eor = 0;
+        for (int cur : arr) {
+            eor ^= cur;
+        }
+
+        int rightOne = eor & (~eor + 1);
+
+        int onlyOne = 0;
+        for (int cur : arr) {
+            if((rightOne & cur) == 1){
+                onlyOne ^= cur;
+            }
+        }
+
+        System.out.println(onlyOne + " " + (eor ^ onlyOne));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,3,4};
+        findOddNumTime2(arr);
+    }
+}
