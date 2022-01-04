4f6024dd39c9bea01f66e482e4d9cfb1a176ba97
liushixing <450973703@qq.com>
2022-01-04 23:36:35 +0800

    ADD practice everyDay 20220104

--- algorithm/src/test/java/com/algorithm/memory/sort/VariousSortAlgorithm.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/VariousSortAlgorithm.java
@@ -774,6 +774,78 @@ public class VariousSortAlgorithm {
         }
     }
 
+    public static void selectSort15(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int i = 0; i < arr.length - 1; i++){
+            int minIndex = i;
+            //内层循环每次都从最左侧加1开始
+            for(int j = i+1; j < arr.length; j++){
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            //找到最小的 就把它放在最左侧
+            NumberUtil.swap(arr,i,minIndex);
+        }
+    }
+    public static void bubbleSort15(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int e = arr.length - 1; e > 0; e--){
+            //内层循环每次到最右侧就是边界 因为最右侧每次都会有一个已经拍好的最大数过去
+            for(int i = 0; i < e; i++){
+                if(arr[i] > arr[i + 1]){
+                    NumberUtil.swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+    public static void insertSort11(int[] arr){
+        if(null == arr || arr.length < 1){
+            return;
+        }
+        for(int i = 1; i < arr.length; i++){
+            for(int j = i - 1; j >=0 && arr[j] > arr[j+1]; j--){
+                NumberUtil.swap(arr,j,j+1);
+            }
+        }
+    }
+
+    public static void mergeSort7(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        process7(arr,0,arr.length-1);
+    }
+
+    private static void process7(int[] arr, int l, int r) {
+        if(l == r){
+            return;
+        }
+        int m = l + ((r - l) >> 1);
+        process7(arr,l,m);
+        process7(arr,m+1,r);
+        mergeArr7(arr,l,m,r);
+    }
+    private static void mergeArr7(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r){
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while(p1 <= m){
+          help[i++] = arr[p1++];
+        }
+        while(p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for(i = 0; i < help.length; i++){
+            arr[l+i] = help[i];
+        }
+    }
 
     public static void main(String[] args) {
         pressureTest();
@@ -786,7 +858,7 @@ public class VariousSortAlgorithm {
         for (int i = 0; i < testTIme; i++) {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
-            insertSort10(arr1);
+            insertSort11(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -799,7 +871,7 @@ public class VariousSortAlgorithm {
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        bubbleSort14(arr);
+        insertSort11(arr);
         PrintUtil.printArr(arr);
     }
 
