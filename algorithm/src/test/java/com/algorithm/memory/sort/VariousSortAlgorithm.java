7702787a22f4a2002be7ea9b425711cf1bac861c
liushixing <450973703@qq.com>
2022-03-23 14:47:41 +0800

    ADD 每日一练20220323  practice 低调

--- algorithm/src/test/java/com/algorithm/memory/sort/VariousSortAlgorithm.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/VariousSortAlgorithm.java
@@ -3386,10 +3386,166 @@ public class VariousSortAlgorithm {
 
 
 
+    //选择排序
+    public static void selectSort37(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        int minIndex;
+        for(int i = 0; i< arr.length - 1; i++){
+            minIndex = i;
+            for(int j = i + 1; j < arr.length;j++){
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            NumberUtil.swap(arr,i,minIndex);
+        }
+    }
+    //冒泡排序
+    public static void bubbleSort37(int[] arr){
+        if(null == arr || arr.length <2){
+            return;
+        }
+        for(int e = arr.length-1; e > 0; e--){
+            for(int i = 0; i < e; i++){
+                if(arr[i] > arr[i+1]){
+                    NumberUtil.swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+    //插入排序
+    public static void insertSort33(int[] arr){
+        if(null == arr || arr.length<2){
+            return;
+        }
+        for(int i = 1;i < arr.length;i++){
+            for(int  j = i -1; j >= 0 && arr[j]>arr[j+1];j--){
+                NumberUtil.swap(arr,j,j+1);
+            }
+        }
+    }
+    //归并排序
+    public static void mergeSort27(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        process27(arr,0,arr.length-1);
+    }
+    private static void process27(int[] arr, int l, int r) {
+        if(l == r){
+            return;
+        }
+        int mid = l + (r - l)/2;
+        process27(arr,l,mid);
+        process27(arr,mid+1,r);
+        mergeLAndR27(arr,l,mid,r);
+    }
+    private static void mergeLAndR27(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = mid+1;
+        int i = 0;
+        while(p1 <= mid && p2 <= r){
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while(p1 <= mid){
+            help[i++] = arr[p1++];
+        }
+        while(p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for(i = 0; i < help.length; i++){
+            arr[l+i] = help[i];
+        }
+    }
+    //快排
+    public static void quickSort16(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        quickSort16(arr,0,arr.length-1);
+    }
+    private static void quickSort16(int[] arr, int l, int r) {
+        if(l < r){
+            int random = l + (int)Math.random() * (r - l);
+            swap(arr,random,r);
+            int[] p = partition17(arr,l,r);
+            quickSort16(arr,l,p[0]-1);
+            quickSort16(arr,p[1]+1,r);
+        }
+    }
+    private static int[] partition17(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while(l < more){
+            if(arr[l] < arr[r]){
+                swap(arr,++less,l++);
+            }else if(arr[l] > arr[r]){
+                swap(arr,--more,l);
+            }else{
+                l++;
+            }
+        }
+        swap(arr,more,r);
+        return new int[]{less+1,more};
+    }
+    //堆排
+    public static void heapSort12(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int i = 0 ; i< arr.length;i++){
+            heapInsert12(arr,i);
+        }
+        int size = arr.length;
+        swap(arr,0,--size);
+        while(size > 0){
+            heapify12(arr,0,size);
+            swap(arr,0,--size);
+        }
+    }
+    public static void heapInsert12(int[] arr,int index){
+        while(arr[index] > arr[(index - 1)/2]){
+            swap(arr,index,(index - 1)/2);
+            index = (index - 1)/2;
+        }
+    }
+    public static void heapify12(int[] arr,int index, int size){
+        int left = index *2 +1;
+        while(left < size){
+            int largest = left+1 < size && arr[left+1] > arr[left] ? left+1 : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if(largest == index){
+                break;
+            }
+            swap(arr,largest,index);
+            index = largest;
+            left = index * 2 +1;
+        }
+    }
 
 
 
-
+    //讲一个数组里面的奇数放左边 偶数放在右边
+    public static void method1(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while(left < right){
+            while(left < right && ((arr[left] % 2 )!= 0)){
+                left++;
+            }
+            while(left < right && ((arr[right] % 2) == 0)){
+                right--;
+            }
+            swap(arr,left,right);
+        }
+    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        method1(arr);
+//        PrintUtil.printArr(arr);
+//    }
 
 
     public static void main(String[] args) {
@@ -3404,7 +3560,7 @@ public class VariousSortAlgorithm {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
             int[] arr3 = {7,3,2,5};
-            heapSort11(arr1);
+            insertSort33(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -3417,7 +3573,7 @@ public class VariousSortAlgorithm {
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        heapSort11(arr);
+        insertSort33(arr);
         PrintUtil.printArr(arr);
     }
 
