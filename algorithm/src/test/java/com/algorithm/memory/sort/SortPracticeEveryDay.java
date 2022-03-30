f5c51fdcec3ced14ea1086a74f2223737351dd41
liushixing <450973703@qq.com>
2022-03-30 14:53:02 +0800

    ADD 每日一练20220330  practice sort everyday

--- algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
@@ -426,8 +426,144 @@ public class SortPracticeEveryDay {
             left = index * 2 + 1;
         }
     }
-
-
+    //改造思路就是加开关，所有跟刷单相关的逻辑都加上开关，这样就不会影响线上逻辑就算合并代码到线上也没事
+    //选择排序
+    public static void selectSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        int minIndex ;
+        for(int i = 0; i < arr.length - 1; i++){
+            minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            NumberUtil.swap(arr,i,minIndex);
+        }
+    }
+    //冒泡排序
+    public static void bubbleSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int e = arr.length - 1; e > 0; e--){
+            for(int i = 0; i < e; i++){
+                if(arr[i] > arr[i+1]){
+                    NumberUtil.swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+    //插入排序
+    public static void insertSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int  i = 1;i < arr.length; i++){
+            for(int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--){
+                NumberUtil.swap(arr,j,j+1);
+            }
+        }
+    }
+    //归并排序
+    public static void mergeSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        process4(arr,0,arr.length-1);
+    }
+    private static void process4(int[] arr, int l, int r) {
+        if(l == r){
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process4(arr,l,mid);
+        process4(arr,mid+1,r);
+        mergeLeftAndRight4(arr,l,mid,r);
+    }
+    private static void mergeLeftAndRight4(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = mid+1;
+        int i = 0;
+        while(p1 <= mid && p2 <= r){
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while(p1 <= mid){
+            help[i++] = arr[p1++];
+        }
+        while(p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for(i = 0; i < help.length; i++){
+            arr[l+i] = help[i];
+        }
+    }
+    //快排
+    public static void quickSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        quickSort4(arr,0,arr.length-1);
+    }
+    private static void quickSort4(int[] arr, int l, int r) {
+        if(l < r){
+            int random = l + (int)Math.random() * (r - l);
+            NumberUtil.swap(arr,random,r);
+            int[] p = partition4(arr,l,r);
+            quickSort4(arr,l,p[0]-1);
+            quickSort4(arr,p[1]+1,r);
+        }
+    }
+    private static int[] partition4(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while(l < more){
+            if(arr[l] < arr[r]){
+                NumberUtil.swap(arr,++less,l++);
+            }else if(arr[l] > arr[r]){
+                NumberUtil.swap(arr,--more,l);
+            }else{
+                l++;
+            }
+        }
+        NumberUtil.swap(arr,more,r);
+        return new int[]{less+1,more};
+    }
+    //堆排
+    public static void heapSort4(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int i = arr.length - 1; i >= 0; i--){
+            heapify4(arr,i,arr.length);
+        }
+        int size = arr.length;
+        NumberUtil.swap(arr,0,--size);
+        while(size > 0){
+            heapify4(arr,0,size);
+            NumberUtil.swap(arr,0,--size);
+        }
+    }
+    public static void heapInsert4(int[] arr, int index){
+        while(arr[index] > arr[(index - 1) / 2]){
+            NumberUtil.swap(arr,index,(index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+    public static void heapify4(int[] arr, int index, int size){
+        int left = index * 2 + 1;
+        while(left < size){
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if(index == largest){
+                break;
+            }
+            NumberUtil.swap(arr,index,largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -442,7 +578,7 @@ public class SortPracticeEveryDay {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
             int[] arr3 = {7,3,2,5};
-            mergeSort3(arr1);
+            heapSort4(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -455,7 +591,7 @@ public class SortPracticeEveryDay {
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        mergeSort3(arr);
+        heapSort4(arr);
         PrintUtil.printArr(arr);
     }
 
