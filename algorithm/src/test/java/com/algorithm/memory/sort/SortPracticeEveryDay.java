27c3823df7ed10853f429eb03bb83adc29ca0150
liushixing <450973703@qq.com>
2022-04-01 13:25:11 +0800

    ADD 每日一练20220401  practice sort everyday

--- algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
@@ -565,6 +565,143 @@ public class SortPracticeEveryDay {
         }
     }
 
+    //选择排序
+    public static void selectSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        int minIndex;
+        for(int i = 0; i < arr.length - 1; i++){
+            minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            NumberUtil.swap(arr,i,minIndex);
+        }
+    }
+    //冒泡排序
+    public static void bubbleSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int e = arr.length - 1; e > 0; e--){
+            for(int i = 0; i < e; i++){
+                if(arr[i] > arr[i+1]){
+                    NumberUtil.swap(arr,i,i+1);
+                }
+            }
+        }
+    }
+    //插入排序
+    public static void insertSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int i = 1; i < arr.length; i++){
+            for(int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--){
+                NumberUtil.swap(arr,j,j+1);
+            }
+        }
+    }
+    //归并排序
+    public static void mergeSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        process5(arr,0,arr.length - 1);
+    }
+    private static void process5(int[] arr, int l, int r) {
+        if(l == r){
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process5(arr,l,mid);
+        process5(arr,mid+1,r);
+        mergeLeftAndRight5(arr,l,mid,r);
+    }
+    private static void mergeLeftAndRight5(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = mid + 1;
+        int i = 0;
+        while(p1 <= mid && p2 <= r){
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while(p1 <= mid){
+            help[i++] = arr[p1++];
+        }
+        while(p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for(i = 0; i < help.length;i++){
+            arr[l+i] = help[i];
+        }
+    }
+    //快排
+    public static void quickSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        quickSort5(arr,0,arr.length-1);
+    }
+    private static void quickSort5(int[] arr, int l, int r) {
+        if(l < r){
+            int random = l + (int)Math.random() * (r - l);
+            NumberUtil.swap(arr,random,r);
+            int[] p = partition5(arr,l,r);
+            quickSort5(arr,l,p[0]-1);
+            quickSort5(arr,p[1] + 1, r);
+        }
+    }
+    private static int[] partition5(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while(l < more){
+            if(arr[l] < arr[r]){
+                NumberUtil.swap(arr,++less,l++);
+            }else if(arr[l] > arr[r]){
+                NumberUtil.swap(arr,--more,l);
+            }else{
+                l++;
+            }
+        }
+        NumberUtil.swap(arr,more,r);
+        return new int[]{less+1,more};
+    }
+    //堆排
+    public static void heapSort5(int[] arr){
+        if(null == arr || arr.length < 2){
+            return;
+        }
+        for(int i = 0; i < arr.length; i++){
+            heapInsert5(arr,i);
+        }
+        int size = arr.length;
+        NumberUtil.swap(arr,0,--size);
+        while(size > 0){
+            heapify5(arr,0,size);
+            NumberUtil.swap(arr,0,--size);
+        }
+    }
+    public static void heapInsert5(int[] arr, int index){
+        while(arr[index] > arr[(index - 1) / 2]){
+            NumberUtil.swap(arr,index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+    public static void heapify5(int[] arr, int index, int size){
+        int left = index * 2 + 1;
+        while(left < size){
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1: left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if(index == largest){
+                break;
+            }
+            NumberUtil.swap(arr,index,largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
 
     public static void main(String[] args) {
         pressureTest();
@@ -578,7 +715,7 @@ public class SortPracticeEveryDay {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
             int[] arr3 = {7,3,2,5};
-            heapSort4(arr1);
+            selectSort5(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -591,7 +728,7 @@ public class SortPracticeEveryDay {
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        heapSort4(arr);
+        selectSort5(arr);
         PrintUtil.printArr(arr);
     }
 
