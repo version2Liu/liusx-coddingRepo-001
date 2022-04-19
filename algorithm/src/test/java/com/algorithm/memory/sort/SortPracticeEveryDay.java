4770e7c42aa7a428be4ad77314914e5446b81f29
liushixing <450973703@qq.com>
2022-04-19 11:08:21 +0800

    ADD 每日一练20220419

--- algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDay.java
@@ -1566,6 +1566,197 @@ public class SortPracticeEveryDay {
         return res;
     }
 
+    //选择排序
+    public static void selectSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            NumberUtil.swap(arr, i, minIndex);
+        }
+    }
+
+    //冒泡排序
+    public static void bubbleSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    NumberUtil.swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    //插入排序
+    public static void insertSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                NumberUtil.swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    //归并排序
+    public static void mergeSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        process10(arr, 0, arr.length - 1);
+    }
+
+    private static void process10(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process10(arr, l, mid);
+        process10(arr, mid + 1, r);
+        mergeLeftAndRight10(arr, l, mid, r);
+    }
+
+    private static void mergeLeftAndRight10(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = mid + 1;
+        int i = 0;
+        while (p1 <= mid && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+
+    //快排
+    public static void quickSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        quickSort10(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort10(int[] arr, int l, int r) {
+        if (l < r) {
+            int random = l + (int) Math.random() * (r - l);
+            NumberUtil.swap(arr, random, r);
+            int[] p = partition10(arr, l, r);
+            quickSort10(arr, l, p[0] - 1);
+            quickSort10(arr, p[1] + 1, r);
+        }
+    }
+
+    private static int[] partition10(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while (l < more) {
+            if (arr[l] < arr[r]) {
+                NumberUtil.swap(arr, ++less, l++);
+            } else if (arr[l] > arr[r]) {
+                NumberUtil.swap(arr, --more, l);
+            } else {
+                l++;
+            }
+        }
+        NumberUtil.swap(arr, more, r);
+        return new int[]{less + 1, more};
+    }
+
+    //堆排
+    public static void heapSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert10(arr, i);
+        }
+        int size = arr.length;
+        NumberUtil.swap(arr, 0, --size);
+        while (size > 0) {
+            heapify10(arr, 0, size);
+            NumberUtil.swap(arr, 0, --size);
+        }
+    }
+
+    public static void heapInsert10(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            NumberUtil.swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify10(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (largest == index) {
+                break;
+            }
+            NumberUtil.swap(arr, index, largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    //桶排序
+    public static void radixSort10(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        int digit = getDigitByArr10(arr);
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketCount = new int[bucket.length];
+
+        for (int d = 1, n = 1; d <= digit; d++, n = n * 10) {
+            //入桶 遍历每一个数字 然后求出当前位数上值 然后入桶
+            for (int i = 0; i < arr.length; i++) {
+                int bucketIndex = (arr[i] / n) % 10;
+                bucket[bucketIndex][bucketCount[bucketIndex]] = arr[i];
+                bucketCount[bucketIndex]++;
+            }
+            int index = 0;
+            for (int i = 0; i < bucket.length; i++) {
+                if (bucketCount[i] == 0) {
+                    continue;
+                }
+                for (int j = 0; j < bucketCount[i]; j++) {
+                    arr[index++] = bucket[i][j];
+                }
+                bucketCount[i] = 0;
+            }
+        }
+    }
+
+    private static int getDigitByArr10(int[] arr) {
+        int max = 0;
+        for (int i : arr) {
+            max = max > i ? max : i;
+        }
+        int res = 0;
+        while (max >= 1) {
+            res++;
+            max = max / 10;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         pressureTest();
     }
@@ -1579,7 +1770,7 @@ public class SortPracticeEveryDay {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
             int[] arr3 = {7, 3, 2, 5};
-            selectSort9(arr1);
+            radixSort10(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -1592,7 +1783,7 @@ public class SortPracticeEveryDay {
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        selectSort9(arr);
+        radixSort10(arr);
         PrintUtil.printArr(arr);
     }
 
