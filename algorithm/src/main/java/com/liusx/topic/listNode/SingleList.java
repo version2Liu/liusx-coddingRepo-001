6e82f39a57e4eaa6dca03619687a58ccef51c5dc
liushixing <450973703@qq.com>
2022-07-24 20:02:26 +0800

    ADD 每日一练7月份第六天

---
+++ algorithm/src/main/java/com/liusx/topic/listNode/SingleList.java
@@ -0,0 +1,13 @@
+package com.liusx.topic.listNode;
+
+/**
+ * @author ：liusx
+ * @date ：Created in 2022/7/23 22:14
+ * @description：单链表结构
+ * @version:
+ */
+public class SingleList<V> {
+    V value;
+    SingleList next;
+
+}
---
+++ algorithm/src/main/java/com/liusx/topic/listNode/exercise/ReverseList.java
@@ -0,0 +1,23 @@
+package com.liusx.topic.listNode.exercise;
+
+/**
+ * @author ：liusx
+ * @date ：Created in 2022/7/23 22:17
+ * @description：反转链表习题
+ * @version:
+ */
+public class ReverseList {
+
+    // 反转单链表
+
+    // 反转双链表
+
+
+    public static void main(String[] args) {
+
+        int random = 3 + (int)(Math.random() * (10 - 3));
+        System.out.println(random);
+    }
+
+
+}
