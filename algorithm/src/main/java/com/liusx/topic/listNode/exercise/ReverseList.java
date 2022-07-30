c8a3dd24a806a0c595b5c5c5f494dcfdb0308099
liushixing <450973703@qq.com>
2022-07-30 10:56:42 +0800

    ADD 反转链表通关联系2

--- algorithm/src/main/java/com/liusx/topic/listNode/exercise/ReverseList.java
+++ algorithm/src/main/java/com/liusx/topic/listNode/exercise/ReverseList.java
@@ -128,7 +128,7 @@ public class ReverseList {
     }
 
     // 不返回节点的反转
-    private static void reverseVoid(ListNode head) {
+    static void reverseVoid(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
@@ -178,10 +178,209 @@ public class ReverseList {
         return head;
     }
 
+
+    // 用迭代的当时解决问题
+    static ListNode reverse0729(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            // 记录当前节点的下一个节点
+            ListNode next = curr.next;
+            // 当前节点下一个指向前一个节点
+            curr.next = prev;
+            // 指针开始向后移动
+            prev = curr;
+            curr = next;
+        }
+        // 返回的是prev 链表反转之后的头结点
+        return prev;
+    }
+
+    static ListNode reverse0729N(ListNode head, int n) {
+        if (n == 1) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (n > 0) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            n--;
+        }
+        // 原来的头结点变成了反转之后的尾结点 尾结点接上剩下的节点 就串起来了一整个节点
+        head.next = curr;
+        // 返回的还是prev 他是反转之后的头结点
+        return prev;
+    }
+
+    static ListNode reverse0729Between(ListNode head, int left, int right) {
+        // 首先记录头结点的位置
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        // 然后找到待排序节点的左边界索引
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        // 再找右边界
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+        // 分割左右区间
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+        // 切断链接
+        pre.next = null;
+        rightNode.next = null;
+        // 对中间区间进行排序
+        reverseVoid(leftNode);
+        // 接腿
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    // 递归方式解决问题
+    static ListNode reverse0729D(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverse0729D(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    static ListNode reverse0729DN(ListNode head, int n) {
+        if (n == 1) {
+            successor0728 = head.next;
+            return head;
+        }
+        ListNode last = reverse0729DN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor0728;
+        return last;
+    }
+
+    static ListNode reverse0729DBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            // 相当于反转前n个数
+            return reverse0729DN(head, n);
+        }
+        head.next = reverse0729DBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+
+    // 迭代方法
+    static ListNode reverse0730(ListNode head) {
+        if (null == head || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static ListNode reverse0730N(ListNode head, int n) {
+        if (n == 1) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (n > 0) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            n--;
+        }
+        // 把排好序的数组末尾 跟 第N+1个节点连接起来
+        head.next = curr;
+        return prev;
+    }
+
+    static ListNode reverse0730Between(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        // 寻找待排序链表的左边界
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        // 寻找待排序链表的右边界
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+        // 截取区间
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+        // 切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 排序
+        reverseVoid(leftNode);
+        // 接腿
+        pre.next = rightNode;
+        leftNode.next = curr;
+
+        return dummyNode.next;
+    }
+
+    // 递归方法
+    static ListNode reverse0730D(ListNode head) {
+        if (null == head || null == head.next) {
+            return head;
+        }
+        ListNode last = reverse0730D(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    static ListNode reverse0730DN(ListNode head, int n) {
+        if (n == 1) {
+            successor0728 = head.next;
+            return head;
+        }
+        ListNode last = reverse0730DN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor0728;
+        return last;
+    }
+
+    static ListNode reverse0730DBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverse0730DN(head, n);
+        }
+        ListNode next = reverse0730DBetween(head.next, m - 1, n - 1);
+        head.next = next;
+        return head;
+
+    }
+
+
     public static void main(String[] args) {
-        ListNode head = NumberUtil.getIntegerListNodeByNum(10);
+        ListNode head = NumberUtil.getIntegerListNodeByNum(15);
         NumberUtil.printListNode(head);
-        ListNode listNode = reverseBetween0728(head,5,8);
+        ListNode listNode = reverse0730DBetween(head,5,11);
         NumberUtil.printListNode(listNode);
     }
+
+
 }
