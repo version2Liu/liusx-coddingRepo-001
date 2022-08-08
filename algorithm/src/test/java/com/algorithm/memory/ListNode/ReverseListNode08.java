5e7e653af17800b8d0d3ff1f238cba65365f7fa4
liushixing <450973703@qq.com>
2022-08-08 21:47:29 +0800

    ADD 指法练习

--- algorithm/src/test/java/com/algorithm/memory/ListNode/ReverseListNode08.java
+++ algorithm/src/test/java/com/algorithm/memory/ListNode/ReverseListNode08.java
@@ -12,13 +12,13 @@ import com.liusx.util.PrintUtil;
  */
 public class ReverseListNode08 {
     // 迭代
-    static ListNode reverse0806(ListNode head){
-        if(null == head || null == head.next){
+    static ListNode reverse0806(ListNode head) {
+        if (null == head || null == head.next) {
             return head;
         }
         ListNode prev = null;
         ListNode curr = head;
-        while(null != curr){
+        while (null != curr) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -27,14 +27,15 @@ public class ReverseListNode08 {
         // 返回翻转过后的头结点
         return prev;
     }
-    static ListNode reverse0806N(ListNode head, int n){
-        if(n == 1){
+
+    static ListNode reverse0806N(ListNode head, int n) {
+        if (n == 1) {
             return head;
         }
         ListNode prev = null;
         ListNode curr = head;
         // 只要前n个数没有反转完 就一直反转
-        while(n > 0){
+        while (n > 0) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -45,19 +46,20 @@ public class ReverseListNode08 {
         head.next = curr;
         return prev;
     }
+
     // 穿针引线法
-    static ListNode reverse0806Between(ListNode head, int left , int right){
+    static ListNode reverse0806Between(ListNode head, int left, int right) {
         // 先把链表之间的关系构建出来
         ListNode dummyNode = new ListNode(-1);
         dummyNode.next = head;
         // 找到左边界
         ListNode pre = dummyNode;
-        for(int i = 0; i < left-1;i++){
+        for (int i = 0; i < left - 1; i++) {
             pre = pre.next;
         }
         // 找右边界
         ListNode rightNode = pre;
-        for(int i = 0; i < right - left + 1;i++){
+        for (int i = 0; i < right - left + 1; i++) {
             rightNode = rightNode.next;
         }
         // 分裂
@@ -70,13 +72,13 @@ public class ReverseListNode08 {
         reverseVoid(leftNode);
         // 接腿
         pre.next = rightNode;
-        leftNode.next=curr;
+        leftNode.next = curr;
         return dummyNode.next;
     }
 
     // 递归
-    static ListNode reverse0806D(ListNode head){
-        if(null == head || null == head.next){
+    static ListNode reverse0806D(ListNode head) {
+        if (null == head || null == head.next) {
             return head;
         }
         ListNode last = reverse0806D(head.next);
@@ -84,9 +86,11 @@ public class ReverseListNode08 {
         head.next = null;
         return last;
     }
-    static ListNode successor ;
-    static ListNode reverse0806DN(ListNode head, int n){
-        if(n == 1){
+
+    static ListNode successor;
+
+    static ListNode reverse0806DN(ListNode head, int n) {
+        if (n == 1) {
             successor = head.next;
             return head;
         }
@@ -95,26 +99,29 @@ public class ReverseListNode08 {
         head.next = successor;
         return last;
     }
-    static ListNode reverse0806DBetween(ListNode head, int m, int n){
-        if(m == 1){
-            return reverse0806DN(head,n);
+
+    static ListNode reverse0806DBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverse0806DN(head, n);
         }
         // 这个地方 递归的结果必须有变量记录，并且需要将 head的next指向这个记录值，是因为m==1的时候的处理结果导致的
         // m==1的时候，是把当前链表的前N个节点全部反转了，反转之后返回的是结果是链表的尾结点，因此需要记录，并且把这个尾结点和头结点接上
         // 因此有下面的两句代码。
         ListNode next = reverse0806DBetween(head.next, m - 1, n - 1);
+        // 注意此处，m和n位置处断开的链表是怎么接上的。注意：n位置的链表接上的操作是在reverse0806DN这个方法里面完成的
+        // 因此我们这里只需要将 递归的结果存储上，然后把m位置的链表接上即可。
         head.next = next;
         return head;
     }
 
 
     private static void reverseVoid(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return;
         }
         ListNode prev = null;
         ListNode curr = head;
-        while(null != curr){
+        while (null != curr) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -122,10 +129,103 @@ public class ReverseListNode08 {
         }
     }
 
+    // 迭代
+    static ListNode reverse0807(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    static ListNode reverse0807N(ListNode head, int n){
+        if(n == 1){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (n >0){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            n--;
+        }
+        // 接上n+1节点
+        head.next = curr;
+        return prev;
+    }
+    static ListNode reverse0807Between(ListNode head, int left ,int right){
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        // 这个地方不要搞错了，节点的初始位置决定了他往下面要走几个节点的距离
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 划定范围
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+        // 切断
+        pre.next = null;
+        rightNode.next = null;
+        // 反转 反转之后 头结点的next就指向空了，所以下面接腿的时候 直接把left.next 接上curr即可。
+        reverseVoid(leftNode);
+        // 接腿
+        pre.next = rightNode;
+        leftNode.next = curr;
+
+        return dummyNode.next;
+    }
+
+    static ListNode reverse0807D(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode last = reverse0807D(head.next);
+        head.next.next= head;
+        head.next = null;
+        return last;
+    }
+    static ListNode reverse0807DN(ListNode head,int n){
+        if(n == 1){
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverse0807DN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+    static ListNode reverse0807DBetween(ListNode head,int m,int n){
+        if(m==1){
+            return reverse0807DN(head,n);
+        }
+        // 此处的处理思路，最终可以简化为一个子问题，那就是反转前n个数的方法，这样就容易理解了
+        // reverse0807DBetween这个方法不算的递归，然后m-1n-1最后发现m=1的时候 就调用反转前n个数的方法，然后不断的进行返回
+        // 最红我们将递归的结果存储住，然后接上m位置就可以了。
+        ListNode next = reverse0807DBetween(head.next, m - 1, n - 1);
+        head.next = next;
+        return head;
+    }
+
+
     public static void main(String[] args) {
         ListNode head = NumberUtil.getIntegerListNodeByNum(16);
         PrintUtil.printListNode(head);
-        ListNode node = reverse0806DBetween(head,6,10);
+        ListNode node = reverse0807DBetween(head,11,15);
         PrintUtil.printListNode(node);
     }
 
--- algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDayMonth08.java
+++ algorithm/src/test/java/com/algorithm/memory/sort/SortPracticeEveryDayMonth08.java
@@ -379,6 +379,182 @@ public class SortPracticeEveryDayMonth08 {
         }
     }
 
+    // 选择排序 时间复杂度O(N2) 空间复杂度 O(1)
+    public static void selectSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            NumberUtil.swap(arr, i, minIndex);
+        }
+    }
+
+    // 冒泡排序 时间复杂度O(n2) 空间复杂度O(1)
+    public static void bubbleSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    // 交换的都是相邻的两个数字
+                    NumberUtil.swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    // 插入排序 时间复杂度O(n2) 空间复杂度O(1)
+    public static void insertSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                NumberUtil.swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    // 归并排序 O(NlogN) 空间复杂度 O(n)
+    public static void mergeSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        process3(arr, 0, arr.length - 1);
+    }
+
+    private static void process3(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process3(arr, l, mid);
+        process3(arr, mid + 1, r);
+        mergeLeftAndRight3(arr, l, mid, r);
+    }
+
+    private static void mergeLeftAndRight3(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int p1 = l;
+        int p2 = mid + 1;
+        int i = 0;
+        while (p1 <= mid && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+
+    // 快排 O(NlogN) O(logN)
+    public static void quickSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        quickSort3(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort3(int[] arr, int l, int r) {
+        if (l < r) {
+            int random = l + (int) (Math.random() * (r - l));
+            NumberUtil.swap(arr, random, r);
+            int[] p = partition3(arr, l, r);
+            quickSort3(arr, l, p[0] - 1);
+            quickSort3(arr, p[1] + 1, r);
+        }
+    }
+
+    private static int[] partition3(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while (l < more) {
+            if (arr[l] < arr[r]) {
+                NumberUtil.swap(arr, ++less, l++);
+            } else if (arr[l] > arr[r]) {
+                NumberUtil.swap(arr, --more, l);
+            } else {
+                l++;
+            }
+        }
+        NumberUtil.swap(arr, more, r);
+        return new int[]{less + 1, more};
+    }
+
+    // 堆排 O(NlogN) O(1)
+    public static void heapSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert3(arr, i);
+        }
+        int size = arr.length;
+        while (size > 0) {
+            NumberUtil.swap(arr, 0, --size);
+            heapify3(arr, 0, size);
+        }
+    }
+
+    private static void heapify3(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (index == largest) {
+                break;
+            }
+            NumberUtil.swap(arr, index, largest);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    private static void heapInsert3(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            NumberUtil.swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    // 桶排序
+    public static void bucketSort3(int[] arr) {
+        if (null == arr || arr.length < 2) {
+            return;
+        }
+        int digit = getDigitByArr(arr);
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketCount = new int[bucket.length];
+        for (int d = 1, n = 1; d <= digit; d++, n = n * 10) {
+            for (int i = 0; i < arr.length; i++) {
+                int bucketIndex = (arr[i] / n) % 10;
+                bucket[bucketIndex][bucketCount[bucketIndex]] = arr[i];
+                bucketCount[bucketIndex]++;
+            }
+            int index = 0;
+            for (int i = 0; i < bucket.length; i++) {
+                if (bucketCount[i] == 0) {
+                    continue;
+                }
+                for (int j = 0; j < bucketCount[i]; j++) {
+                    arr[index++] = bucket[i][j];
+                }
+                bucketCount[i] = 0;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         pressureTest();
@@ -393,7 +569,7 @@ public class SortPracticeEveryDayMonth08 {
             int[] arr1 = NumberUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = NumberUtil.copyArray(arr1);
             int[] arr3 = {7, 3, 2, 5};
-            heapSort2(arr1);
+            bucketSort3(arr1);
             NumberUtil.comparator(arr2);
             if (!NumberUtil.isEqual(arr1, arr2)) {
                 succeed = false;
@@ -402,12 +578,12 @@ public class SortPracticeEveryDayMonth08 {
                 break;
             }
         }
-        String method = "heapSort2";
+        String method = "bucketSort3";
         System.out.println(succeed ? method + " --> 完美通过。你真是太帅了！！！！！！！！！！！" : "Oh no u are fail!");
 
         int[] arr = NumberUtil.generateRandomArray(maxSize, maxValue);
         PrintUtil.printArr(arr);
-        heapSort2(arr);
+        bucketSort3(arr);
         PrintUtil.printArr(arr);
     }
 
