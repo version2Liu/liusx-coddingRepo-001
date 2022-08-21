7be8ae5b6037820c2da63790ad46693d14ad3446
liushixing <450973703@qq.com>
2022-08-21 17:38:01 +0800

    ADD ListNode reverse practice everyday

--- algorithm/src/test/java/com/algorithm/memory/ListNode/ReverseListNode08.java
+++ algorithm/src/test/java/com/algorithm/memory/ListNode/ReverseListNode08.java
@@ -214,18 +214,118 @@ public class ReverseListNode08 {
             return reverse0807DN(head,n);
         }
         // 此处的处理思路，最终可以简化为一个子问题，那就是反转前n个数的方法，这样就容易理解了
-        // reverse0807DBetween这个方法不算的递归，然后m-1n-1最后发现m=1的时候 就调用反转前n个数的方法，然后不断的进行返回
+        // reverse0807DBetween这个方法不断的递归，然后m-1n-1最后发现m=1的时候 就调用反转前n个数的方法，然后不断的进行返回
         // 最红我们将递归的结果存储住，然后接上m位置就可以了。
         ListNode next = reverse0807DBetween(head.next, m - 1, n - 1);
         head.next = next;
         return head;
     }
 
+    // 反转链表
+    static ListNode reverse0821(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    static ListNode reverse0821N(ListNode head, int n){
+        if(n == 1){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while(n > 0){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            n--;
+        }
+        head.next = curr;
+        return prev;
+    }
 
+    static ListNode reverse0821Between(ListNode head,int left, int right){
+        // 找到一个虚拟节点
+        ListNode dummyNode = new ListNode(-1);
+        // 与当前链表挂载上
+        dummyNode.next = head;
+        // 定义第一个节点，用来寻找待排序链表的左侧位置
+        ListNode pre = dummyNode;
+        // 找到左边界
+        for(int i = 0; i < left - 1; i++){
+            pre = pre.next;
+        }
+        // 找到右边界
+        ListNode rightNode = pre;
+        for(int i = 0; i < right-left+1;i++){
+            rightNode = rightNode.next;
+        }
+
+        // 切割
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+        // 断开链接
+        pre.next = null;
+        rightNode.next=null;
+        // 反转
+        reverseVoid(leftNode);
+        // 接腿
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    static ListNode reverse0821D(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        // 把当前链表的 后半部分全部反转 返回反转后的链表头结点
+        ListNode last = reverse0821D(head.next);
+        // 将反转过的链表 和 头结点的关系重新构建 需要把头结点挂载到链表的最后
+        head.next.next = head;
+        // 并把链表的最后节点置空
+        head.next = null;
+        // 返回反转之后的链表的头结点
+        return last;
+    }
+
+    static ListNode reverse0821DN(ListNode head, int n){
+        if(n ==1){
+            successor = head.next;
+            return head;
+        }
+        // 链表的第一节点之后的所有节点反转
+        ListNode last = reverse0821DN(head.next, n - 1);
+        // 带上第一个节点
+        head.next.next = head;
+        // 把排好序的节点和 后续的节点连接上
+        head.next = successor;
+        return last;
+    }
+    static ListNode reverse0821DBetween(ListNode head, int m, int n){
+        if(m == 1){
+            return reverse0821DN(head,n);
+        }
+        // 递归直到问题转化为 反转链表的前N个节点
+        ListNode next = reverse0821DBetween(head.next, m - 1, n - 1);
+        //
+        head.next = next;
+        return head;
+
+    }
     public static void main(String[] args) {
         ListNode head = NumberUtil.getIntegerListNodeByNum(16);
         PrintUtil.printListNode(head);
-        ListNode node = reverse0807DBetween(head,11,15);
+        ListNode node = reverse0821DBetween(head,6,11);
         PrintUtil.printListNode(node);
     }
 
